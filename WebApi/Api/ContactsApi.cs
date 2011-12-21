e5e4e4ecb5999810e2112ce1640d4dba90414bc0
mhamrah <mhamrah@gmail.com>
2011-12-21 15:23:58 -0500

    Initial Commit

---
+++ WebApi/Api/ContactsApi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace WebApi.Api
+{
+    [ServiceContract]
+    public class ContactsApi
+    {
+        [WebGet(UriTemplate = "")]
+        public IEnumerable<Contact> Get()
+        {
+            var contacts = new List<Contact>()
+                               {
+                                   new Contact() {ContactId = 1, Name = "Ron Grontowski"},
+                                   new Contact() {ContactId = 1, Name = "Tom Brady"},
+
+                               };
+            return contacts;
+        } 
+    }
+}
